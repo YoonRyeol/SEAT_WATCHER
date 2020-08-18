e61bfcfa3654793bd08b90aae9f54f0fde20e5c4
YoonRyeol <yunyul3@gmail.com>
2020-08-18 17:23:26 +0900

    2020-08-18 | YR | table_set not group

--- watcher/views.py
+++ watcher/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from watcher.models import *
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from watcher.tools import *
 
+# Create your views here.
 
 def Camera_list(request) :
 	return render(request, 'watcher/Camera_list.html')
 
-
 def table_set(request):
 	return render(request, 'watcher/table_set_fabric.html')
+
+@csrf_exempt
+def image_test(request):
+	handle_uploaded_file(request.FILES['file'])
