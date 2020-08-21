d5d69ec8770164484eb50447f3d97052d46fd638
YoonRyeol <yunyul3@gmail.com>
2020-08-21 13:22:08 +0900

    2020-08-21 | YR | 테이블 데이터 생성 구현

--- watcher/apis.py
+++ watcher/apis.py
@@ -4,11 +4,43 @@ import requests
 import json
 
 def send_seat_data(request):
-    string_data = request.POST['seat_data']
-    real_data = json.loads(string_data)
-    r = requests.post('http://localhost:8010/get_seat_info', data = {'seat_data':string_data}, timeout=5)
-    print(real_data)
-    return HttpResponse('')
+	"""
+	Todo : get camera id, store id
+	"""
+	string_data = request.POST['seat_data']
+	real_data = json.loads(string_data)
+	"""
+	real data format
+	{
+		'is_elec': False, 
+		'capacity': '1', 
+		'position': {'f_x': 0.19416666666666665, 'f_y': 0.35, 's_x': 0.2808333333333333, 's_y': 0.46555555555555556}
+	}
+	"""
+
+	"""
+	Todo : cam addr setting
+	"""
+	try:
+		response = requests.post('http://localhost:8010/get_seat_info', data={'seat_data':string_data}, timeout=5)
+	except Exception:
+		return HttpResponse('connection error')
+	if response.text != 'good':
+		return HttpResponse('connection error2')
+	
+	for elem in real_data:
+		target_data = {
+			'pic_f_x' : elem['position']['f_x'],
+			'pic_f_y' : elem['position']['f_y'],
+			'pic_s_x' : elem['position']['s_x'],
+			'pic_s_y' : elem['position']['s_y'],
+			'is_elec' : elem['is_elec'],
+			'capacity' : elem['capacity']
+		}
+		Table.objects.create(**target_data)
+
+
+	return HttpResponse('good')
 
 def get_data(request):
 	pk = int(request.GET['pk'])
--- watcher/views.py
+++ watcher/views.py
@@ -9,6 +9,12 @@ def Camera_list(request) :
 	return render(request, 'watcher/Camera_list.html')
 
 def table_set(request):
+	"""
+	Todo : 사진 데이터 로드
+	"""
+	"""
+	Todo : 좌석데이터 로드
+	"""
 	return render(request, 'watcher/table_set_fabric.html')
 
 @csrf_exempt
