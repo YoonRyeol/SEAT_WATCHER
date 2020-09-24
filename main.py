ca4e55453a49d56cd3f393be5cb0782363f6d24a
JiBeom <mayer0425@naver.com>
2020-09-24 20:09:56 +0100

    200925

--- main.py
+++ main.py
@@ -7,42 +7,56 @@ import time
 import json
 import requests
 
-cap = cv2.VideoCapture(0)
-
-#frame_width = int(cap.get(3))
-#frame_height = int(cap.get(4))
 
 capture = False
-isStart = False
 
-def initializeResult():
+def initialize():
+    print("init....")
+    with open('pos_data.json', 'r') as json_file:
+        data = json.load(json_file)
+
+
     result = []
-    tempResult = []
+    table_status = []
     for elem in data:
-        tempResult = {'pk' : data['pk'], 'res' : 'F'}
-        result.update(tempResult)
-    with open('result.json', 'w') as make_file:
-        json.dump(tempResult, make_file)
-    
+        result.append({"pk" : elem['pk'], "res" : "F"})
+        elem['flags'] = "F"
+        table_status.append(0)
 
+    with open('result.json', 'w') as make_file:
+        json.dump(result, make_file, indent = 4)
+    with open('pos_data.json', 'w') as json_file:
+        json.dump(data, json_file, indent = 4)
+        
+    
+def updateCheck(data):
+    if(data['flags'] == "T"):
+        return True
+    
 def save_result_json():
     with open('result.json', 'w') as make_file:
-        json.dump(result, make_file)
+        json.dump(result, make_file, indent = 4)
         url = 'http://18.219.121.103:8001/api/get_seat_inspection_result'
-        send_data = { 'input' : json.dumps(result)}
+        send_data = { 'input' : json.dumps(result, indent = 4)}
         print("post :: " + str(result))
         r = requests.post(url, data=send_data)
 
+def swap(x1, x2):
+    v_swap = 0
+    if(x1 > x2):
+        v_swap = x1
+        x1 = x2
+        x2 = v_swap
+        return x1, x2
 
 def ROI(x1, x2, y1, y2, pk):
-
     captured = cv2.imread("images/30.jpg", 1)
     spot1, spot2 = [int(round(captured.shape[0] * y1)), int(round(captured.shape[1] * x1))], [int(round(captured.shape[0] * y2)), int(round(captured.shape[1] * x2))]
-
     if os.path.isfile("images/captured_"+str(pk)+".jpg"):
         captured = captured[spot1[0]:spot2[0], spot1[1]:spot2[1]]
         cv2.imwrite("images/captured_"+str(pk)+".jpg", captured)
         return(captured)
+
 
     origin = cv2.imread("images/origin.jpg", 1)
     origin = origin[spot1[0]:spot2[0], spot1[1]:spot2[1]]
@@ -96,54 +110,53 @@ def is_there_seat(x1, x2, y1, y2, pk):
         return False
 
 
-if os.path.isfile("images/origin.jpg"):
-    isStart = True
-
 table_status = []
+with open('pos_data.json', 'r') as json_file:
+    data = json.load(json_file)
+initialize()
+with open('result.json', 'r') as json_file:
+    result = json.load(json_file)
     
-while (True):
-    ret, frame = cap.read()
+cap = cv2.VideoCapture(0)
+#frame_width = int(cap.get(3))
+#frame_height = int(cap.get(4))
+    
+while True:
     now = datetime.datetime.now().strftime("%S")
     now = str(now)
-
-    if ret == True:
+    ret, frame = cap.read()
+    if ret :
         # Display the resulting frame
-        cv2.imshow('frame', frame)
+        cv2.imshow('cam', frame)
         if not os.path.isfile("images/origin.jpg"):
-            time.sleep(2)
             cv2.imwrite("images/origin.jpg", frame)
-        if((now == "00" or now == "20" or now == "40") and capture != True and isStart is not False) :
+            time.sleep(2)
+        if((now == "00" or now == "20" or now == "40") and capture != True) :
             cv2.imwrite("images/30.jpg", frame)
-            print("30 is start")
-            
+            print("detection loop is start....")
             with open('pos_data.json', 'r') as json_file:
-                data = json.load(json_file)
-            with open('result.json', 'r') as json_file:
-                result = json.load(json_file)
-                    
-            #    print(data["t1"]["x1"])
-            #    print(len(json_data))
-            #    print(json_data['percentage']['x1'])
-
-
+                data = json.load(json_file)                                
             status_index = 0
             for elem in data:
-                table_status.append(0)
-                    
-            for elem in data:
+                if(elem['flags']):
+                    initialize()
+                    print('\'pos_data.json\' has been changed... restart loop')
+                    break                
                 x1 = elem['position']['f_x']
                 x2 = elem['position']['s_x']
                 y1 = elem['position']['f_y']
                 y2 = elem['position']['s_y']
+                x1, x2 = swap(x1, x2)
+                y1, y2 = swap(y1, y2)
                 print("axis", x1, x2, y1, y2)
                 captured = ROI(x1, x2, y1, y2, elem['pk'])
                 if(is_there_seat(x1, x2, y1, y2, elem['pk'])):
                     if(table_status[status_index] >= 1):
-                        if(result[status_index]['res'] == "T"):
-                            result[status_index]['res'] = "F"
+                        if(result[str(elem['pk'])]['res'] == "T"):
+                            result[str(elem['pk'])]['res'] = "F"
                             save_result_json();
                         else:
-                            result[status_index]['res'] = "T"
+                            result[str(elem['pk'])]['res'] = "T"
                             save_result_json();
                             print("table " + str(elem['pk']) + " is diff")
                         table_status[status_index] = 0    
@@ -163,11 +176,12 @@ while (True):
             capture = True
         if(now == "31"):
             capture = False
+        if cv2.waitKey(1) & 0xFF == 27 :
+            break
+            
     # Break the loop
     else:
         break
-    time.sleep(1)
 
 cap.release()
-cv2.waitKey(0)
 cv2.destroyAllWindows()
